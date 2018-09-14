bb6c6271fbf707607a3990b35c34e7099c95e753
= <=>
2018-09-14 15:56:29 +0700

    [Thực hành] Triển khai stack sử dụng lớp LinkedList trong thư viện java.util

---
+++ src/test/java/LinkedListStackTest.java
@@ -0,0 +1,41 @@
+import javax.xml.bind.SchemaOutputResolver;
+
+public class LinkedListStackTest {
+    private static void stackOfStrings() {
+        MyGenericStack<String> stack = new MyGenericStack<>();
+        stack.push("Five");
+        stack.push("Four");
+        stack.push("Three");
+        stack.push("Two");
+        stack.push("One");
+        System.out.println("1.1. Size of stack after push operations: " + stack.size());
+        System.out.println("1.2. Pop elements from stack : ");
+        while (!stack.isEmpty()) {
+            System.out.printf("%s", stack.pop());
+        }
+        System.out.println("\n1.3. Size of stack after pop operations: " + stack.size());
+
+    }
+    private static void stackOfItergers() {
+        MyGenericStack<Integer> stack = new MyGenericStack<>();
+        stack.push(5);
+        stack.push(4);
+        stack.push(3);
+        stack.push(2);
+        stack.push(1);
+        System.out.println("1.1. Size of stack after push operations: " + stack.size());
+        System.out.println("1.2. Pop elements from stack : ");
+        while (!stack.isEmpty()) {
+            System.out.printf("%s", stack.pop());
+        }
+        System.out.println("\n1.3. Size of stack after pop operations: " + stack.size());
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("1. Stack of integers");
+        stackOfItergers();
+        System.out.println("\n2. Stack of Strings");
+        stackOfStrings();
+    }
+}
